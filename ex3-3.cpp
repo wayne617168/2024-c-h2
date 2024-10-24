bedb28573675f59f6f395f9327bbe4bd846a20ad
wayne617168 <930799@st.tc.edu.tw>
2024-10-24 20:41:25 +0800

    Create ex3-3.cpp

---
+++ ex3-3.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+  //This is comment by Wayne
+    printf("Hello World");//This is comment
+    
+                                                   
+   return0
+}
