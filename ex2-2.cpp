5aac837174cb15aaeb70fac4a65d6b0c8c6c71d0
wayne617168 <930799@st.tc.edu.tw>
2024-10-24 19:50:49 +0800

    Update ex2-2.cpp

--- ex2-2.cpp
+++ ex2-2.cpp
@@ -1,1 +1,9 @@
+#include <stdio.h>
 
+int main()
+{
+    printf("Hello World");
+    printf("This sentence will work");
+    printf("I like coding");                                                
+   return 0;
+}
